5213c2fde345d0f2552a2ac48ca48ab56dec4ba8
Francis Nguyen <mrcampust70@gmail.com>
2017-04-26 12:11:41 -0700

    Bowling Run

---
+++ Bowling Run/Assets/Scripts/ExitScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour
+{
+
+    void Update() //Simple command to end game with "esc key"
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+}
---
+++ Bowling Run/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 300, 80), "Start Game"))
+        {
+        
+            SceneManager.LoadScene("Scene1", LoadSceneMode.Single);
+        }
+    }
+}
