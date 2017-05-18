e5fd9cd285ee93ff6e71fe57270d2940f7ccdb54
Francis Nguyen <mrcampust70@gmail.com>
2017-05-18 12:02:03 -0700

    Camera
    Camera now follows the bowling ball, working on respawn script and have
    begun work on the levels.

---
+++ Bowling Run/Assets/Scripts/Respawn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour
+{
+
+    // Use this for initialization
+    private void Start()
+    {
+        GameObject Player = GameObject.Find("Player");
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position = Player.transform.position;
+        }
+    }
+}
---
+++ Bowling Run/Assets/Scripts/Static.js
@@ -0,0 +1,5 @@
+﻿#pragma strict
+
+function Update () {
+    transform.eulerAngles = Vector3(0, 0, 0);
+}
